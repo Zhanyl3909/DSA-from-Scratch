e6640740f9d6b6467a614358ddd3b83ffc8e1837
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-06-10 09:15:58 +0900

    Update Main.java

--- Binary_Search/Main.java
+++ Binary_Search/Main.java
@@ -9,3 +9,15 @@ if(x == S[mid]) location = mid;
 else if(x < S[mid]) high = mid -1;  //if the mid is more than x
 else low = mid + 1; //if the x is more than mid 
 }
+
+  
+  //recursive 
+  index binarySearch(keytpype S[], keytype x, index low, index high) 
+{
+	index mid = (low+high)/2;
+	if(low > high) return 0;
+	else 
+	 if(x == S[mid]) return mid;
+	 else if(x < S[mid]) return binarySearch(S,x,low,mid-1);
+	 else return binarySearch(S,x,mid+1,high);
+}
