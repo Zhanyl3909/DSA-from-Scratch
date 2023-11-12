4a1f417ef86816bd8db1090789ef8e46871ba41f
Zhanylai <87288278+Zhanyl3909@users.noreply.github.com>
2023-11-12 18:16:31 +0900

    Update Main.java

---
+++ Stack/Main.java
@@ -0,0 +1,31 @@
+import java.util.Stack;  
+
+public class Main {
+    public static void main(String[] args) {
+        //Stack Create
+
+        //Declaring Stack
+        Stack<String> stackk = new Stack<>();
+
+        if(stackk.empty() == true) {
+            System.out.println("The empty stack");
+        } else {
+            System.out.println("This is not empty");
+        }
+
+
+        //pushing items into Stack
+        stackk.push("Hello");
+        stackk.push("World");
+        stackk.push("I am Seriously going to Crack DSA");
+        stackk.push("Hello");
+        stackk.push("Bue");
+        stackk.pop();
+
+        System.out.println(stackk.empty());
+        System.out.println(stackk);
+
+        System.out.println(stackk.search("Hello"));  //it returns int the order inside the stack, and if there isn't anything it returns-1
+                                                        //in case there are a lot of same value, it is going to return nearest one
+    }
+}
